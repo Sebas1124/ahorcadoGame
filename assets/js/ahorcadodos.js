a7871b5dcf2f213e59b5ed064f8de1a811e6c5b3
javier lombardero <javier.lombar2@gmail.com>
2024-07-19 11:08:56 +0200

    nuevos cambios

--- assets/js/ahorcadodos.js
+++ assets/js/ahorcadodos.js
@@ -1,46 +1,38 @@
-const modalInicio = document.querySelector(".intro")
-const botonInicio  = document.querySelector(".intro button")
-const inputInicio  = document.querySelector(".intro form input")
-const campos = document.querySelector("#campos")
+const modalInicio = document.querySelector(".intro");
+const botonInicio = document.querySelector(".intro button");
+const inputInicio = document.querySelector(".intro form input");
+const campos = document.querySelector("#campos");
 
+botonInicio.addEventListener("click", () => {
+  let palabraElegida = inputInicio.value;
 
-
-
-botonInicio.addEventListener("click", ()=>{
-  let palabraElegida =  inputInicio.value
-if(palabraElegida == ""){
-        alert("debes introducir una palabra para poder empezar")
-    }else if( isNaN(palabraElegida) == false){
-       alert("solo se permiten caracteres alfabéticos")
-    }else{
+  if (palabraElegida == "") {
+    alert("debes introducir una palabra para poder empezar");
+  } else if (isNaN(palabraElegida) == false) {
+    alert("solo se permiten caracteres alfabéticos");
+  } else {
     modalInicio.style.display = "none";
-    }
-   const arrayLetras =  palabraElegida.split("")
-   const nuevosInputs = `<input type="text">`
-   arrayLetras.forEach(()=>{
-       campos.insertAdjacentHTML("beforeend", nuevosInputs)
-    })
-    return arrayLetras
-   }  
-)
+  }
 
-const camposInputs = campos.querySelectorAll("input")
+  const arrayLetras = palabraElegida.split("");
+  const nuevosInputs = `<input type="text">`;
 
-camposInputs.forEach((input)=>{
-  input.addEventListener("input",()=>{
+  arrayLetras.forEach(() => {
+    campos.insertAdjacentHTML("beforeend", nuevosInputs);
+  });
+  return arrayLetras;
+});
+
+
+
+nuevosInputs.forEach((input, i)=>{
+  input.addEventListener("change",()=>{
     for(i=0; i < arrayLetras.length; i++){
-       if(input.value === arrayLetras[i]){
-      input.textContent = arrayLetras[i].toString("")
+       if(input.value == arrayLetras[i]){
+      input.textContent = arrayLetras[i]
     }else{
-        alarm("has fallado")
+        alert("has fallado")
     }
   }}
 )})
-
-
-
-    
-
-
-
 
